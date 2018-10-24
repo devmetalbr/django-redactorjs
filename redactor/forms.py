51b7f005f9ee4ec8eeeb3e5ff383c32f83d4a775
722C <722C@users.noreply.github.com>
2018-10-24 11:15:14 -0600

    Use standard BooleanFields.

--- redactor/forms.py
+++ redactor/forms.py
@@ -3,9 +3,9 @@ from django import forms
 
 class ImageForm(forms.Form):
     file = forms.ImageField()
-    overwrite = forms.NullBooleanField(required=False)
+    overwrite = forms.BooleanField(required=False)
 
 
 class FileForm(forms.Form):
     file = forms.FileField()
-    overwrite = forms.NullBooleanField(required=False)
+    overwrite = forms.BooleanField(required=False)
